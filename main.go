9861384b1274e6b9e9b20354122b8e4c7fcd2344
xixizeng <202618236@qq.com>
2023-07-08 15:37:44 +0800

    初次提交

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "ginchat/test"
+
+func main() {
+	test.IntTest()
+}
---
+++ test/testGorm.go
@@ -0,0 +1,32 @@
+package test
+
+import (
+	"fmt"
+	"ginchat/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
+func IntTest() {
+
+	dsn := "root:123456@tcp(127.0.0.1:3307)/ginchat?charset=utf8&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&models.UserBasic{})
+	user := &models.UserBasic{}
+	user.Name = "申专"
+	db.Create(user)
+
+	fmt.Println(db.First(user, 1))
+	//db.First(user, "code = ?", "D42")
+	//db.Model(user).Update("Price", 200)
+	db.Model(user).Update("PassWord", "1234")
+}
