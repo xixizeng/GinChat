6a0df025020062e3aca656eb102eb1b4292d5bf6
xixizeng <202618236@qq.com>
2023-07-10 22:13:24 +0800

    添加swagger

--- router/app.go
+++ router/app.go
@@ -8,6 +8,15 @@ import (
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @Summary 测试index
+// @Description 用来测试swagger
+// @Tags
+// @Accept json
+// @Produce json
+// @Success 200 {string} welcome
+// @Fialure 500 {string} notwelcom
+// @Router /index [get]
+
 func Router() *gin.Engine {
 
 	router := gin.Default()
--- utils/system_init.go
+++ utils/system_init.go
@@ -5,6 +5,10 @@ import (
 	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"time"
 )
 
 var DB *gorm.DB
@@ -21,8 +25,17 @@ func InitConfig() {
 }
 func InitMysql() {
 	var err error
+	//自定义日志模板打印sql语句
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second, //慢sql阈值
+			LogLevel:      logger.Info, //级别
+			Colorful:      true,        //彩色
+		},
+	)
 
-	DB, err = gorm.Open(mysql.Open(viper.GetString("mysql.dns")), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open(viper.GetString("mysql.dns")), &gorm.Config{Logger: newLogger})
 	if err != nil {
 		panic("failed to connect database")
 	}
